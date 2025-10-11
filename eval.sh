470acd5618bcfc7b9e78d49f9520353c1f3109c4
SwayStar123 <swayambhanded@gmail.com>
2025-10-11 20:00:11 +0000

    add 50 NFE results

--- eval.sh
+++ eval.sh
@@ -2,8 +2,8 @@
 random_number=$((RANDOM % 100 + 1200))
 NUM_GPUS=8
 STEP="0100000"
-SAVE_PATH="exps/xl1-reg-invae"
-NUM_STEP=250
+SAVE_PATH="exps/xl1-reg-invae-tread-delayed"
+NUM_STEP=50
 MODEL_SIZE='XL'
 CFG_SCALE=1.0
 CLS_CFG_SCALE=1.0
