63188df07ef449006faac45a60354c666b379ad9
SwayStar123 <swayambhanded@gmail.com>
2025-11-24 14:36:12 +0000

    make default max steps 400k

--- train.sh
+++ train.sh
@@ -17,4 +17,6 @@ accelerate launch --multi_gpu --num_processes $NUM_GPUS train.py \
     --exp-name="b1-reg-invae" \
     --batch-size=256 \
     --data-dir="dataset" \
-    --cls=0.03
+    --cls=0.03 \
+    --resume-step=120000
+    --
