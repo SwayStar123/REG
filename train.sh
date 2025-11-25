4bd6f3dc17769b123a986f0b3ebe4d57e5c1649f
SwayStar123 <swayambhanded@gmail.com>
2025-11-25 18:17:16 +0000

    add rmsnorm

--- train.sh
+++ train.sh
@@ -14,7 +14,7 @@ accelerate launch --multi_gpu --num_processes $NUM_GPUS train.py \
     --enc-type="dinov2-vit-b" \
     --proj-coeff=0.5 \
     --output-dir="exps" \
-    --exp-name="b1-reg-invae-sprint-swiglu" \
+    --exp-name="b1-reg-invae-sprint-swiglu-rmsnorm" \
     --batch-size=256 \
     --data-dir="dataset" \
     --cls=0.03
