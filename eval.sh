2821c4b0c18e500d25f533db11674311a706b416
SwayStar123 <swayambhanded@gmail.com>
2026-01-22 16:13:50 +0000

    add xl results

--- eval.sh
+++ eval.sh
@@ -1,9 +1,9 @@
 random_number=$((RANDOM % 100 + 1200))
 NUM_GPUS=8
 STEP="0400000"
-SAVE_PATH="exps/srdit-12+improved-immiscible-diffusion"
+SAVE_PATH="exps/srdit-12+improved-immiscible-diffusion-xl"
 NUM_STEP=250
-MODEL_SIZE='B'
+MODEL_SIZE='XL'
 CFG_SCALE=1.0
 CLS_CFG_SCALE=1.0
 GH=1.0
--- train.sh
+++ train.sh
@@ -9,11 +9,11 @@ accelerate launch --multi_gpu --num_processes $NUM_GPUS train.py \
     --path-type="linear" \
     --prediction="v" \
     --weighting="uniform" \
-    --model="SiT-B/1" \
+    --model="SiT-XL/1" \
     --enc-type="dinov2-vit-b" \
     --proj-coeff=0.5 \
     --output-dir="exps" \
-    --exp-name="srdit-12+improved-immiscible-diffusion" \
+    --exp-name="srdit-12+improved-immiscible-diffusion-xl" \
     --batch-size=256 \
     --data-dir="dataset" \
     --cls=0.03 \
